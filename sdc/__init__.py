134eccc9ea6cfd8802a564871a2adaa57520c24e
maawoo <marco.wolsza@uni-jena.de>
2023-10-20 14:53:43 +0200

    [sdc] execute `sdc.cluster.start_slurm_cluster` on package import

--- sdc/__init__.py
+++ sdc/__init__.py
@@ -1,5 +1,11 @@
+import warnings
 import dask
 import xarray as xr
+from sdc.cluster import start_slurm_cluster
 
 dask.config.set({'array.chunk-size': '256MiB'})
 xr.set_options(keep_attrs=True)
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+    dask_client, cluster = start_slurm_cluster()
