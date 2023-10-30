2269e35f0ffe763826314e3e9255bb528c16dcaa
maawoo <marco.wolsza@uni-jena.de>
2023-10-30 14:44:25 +0100

    [sdc.load] add option: `group_acq_slices`

--- sdc/load.py
+++ sdc/load.py
@@ -4,6 +4,7 @@ from typing import Optional, Tuple
 from xarray import Dataset
 
 from sdc.vec import get_site_bounds
+from sdc.utils import groupby_acq_slices
 from sdc.s1 import load_s1_rtc
 from sdc.s2 import load_s2_l2a
 
@@ -12,7 +13,8 @@ def load_product(product: str,
                  vec: str, 
                  time_range: Optional[Tuple[str, str]] = None,
                  time_pattern: Optional[str] = '%Y-%m-%d',
-                 apply_mask: bool = True) -> Dataset:
+                 apply_mask: bool = True,
+                 group_acq_slices: bool = True) -> Dataset:
     """
     Load data products available in the SALDi Data Cube (SDC).
     
@@ -35,6 +37,9 @@ def load_product(product: str,
         Whether to apply a quality mask to the data. Default is True. The specifics of
         the mask depend on the product. See the documentation of the specific product
         for details.
+    group_acq_slices : bool, optional
+        Whether to group acquisition slices. Default is True. For more details, see the
+        documentation of `utils.group_acq_slices`.
     """
     if vec.lower() in ['site01', 'site02', 'site03', 'site04', 'site05',  'site06']:
         bounds = get_site_bounds(site=vec.lower())
@@ -52,5 +57,8 @@ def load_product(product: str,
         ds = load_s2_l2a(**kwargs)
     else:
         raise ValueError(f'Product {product} not supported')
+    
+    if group_acq_slices:
+        ds = groupby_acq_slices(ds)
     
     return ds
