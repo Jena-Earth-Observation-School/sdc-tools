d0670443438f444dc8473fa1cb3a303b4c371e41
maawoo <marco.wolsza@uni-jena.de>
2023-11-09 14:10:43 +0100

    [sdc] use default value for dask `array.chunk-size` (128MiB)

--- sdc/__init__.py
+++ sdc/__init__.py
@@ -1,8 +1,6 @@
-import dask
 import xarray as xr
 from sdc.cluster import start_slurm_cluster
 
-dask.config.set({'array.chunk-size': '256MiB'})
 xr.set_options(keep_attrs=True)
 
 dask_client, cluster = start_slurm_cluster()
