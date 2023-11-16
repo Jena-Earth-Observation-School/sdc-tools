93fb2ff731c713739feaedb073c930e040f888fc
maawoo <marco.wolsza@uni-jena.de>
2023-11-16 13:59:52 +0100

    [sdc.load] add SANLC product

---
+++ sdc/sanlc.py
@@ -0,0 +1,42 @@
+from pystac import Catalog
+from odc.stac import load as odc_stac_load
+
+from typing import Tuple
+from xarray import Dataset
+
+import sdc.utils as utils
+import sdc.query as query
+
+
+def load_sanlc(bounds: Tuple[float, float, float, float],
+                ) -> Dataset:
+    """
+    Loads the South African National Land Cover (SANLC) data product for an area of 
+    interest.
+    
+    Parameters
+    ----------
+    bounds: tuple of float
+        The bounding box of the area of interest in the format (minx, miny, maxx, maxy).
+        Will be used to filter the STAC Catalog for intersecting STAC Collections.
+    
+    Returns
+    -------
+    Dataset
+        An xarray Dataset containing the SANLC data.
+    """
+    product = 'sanlc'
+    bands = ['nlc']
+    
+    # Load and filter STAC Items
+    catalog = Catalog.from_file(utils.get_catalog_path(product=product))
+    _, items = query.filter_stac_catalog(catalog=catalog, bbox=bounds)
+    
+    common_params = utils.common_params()
+    common_params['resampling'] = 'nearest'
+    
+    # Turn into dask-based xarray.Dataset
+    ds = odc_stac_load(items=items, bands=bands, bbox=bounds, dtype='uint8',
+                       **common_params)
+    
+    return ds.nlc.squeeze()
