53cd77115c51a12a70ff99052d97d62e8a6523f2
maawoo <marco.wolsza@uni-jena.de>
2023-11-29 17:00:35 +0100

    [sdc.products.sanlc] new year available: 2018; allow loading years separately

--- sdc/products/sanlc.py
+++ sdc/products/sanlc.py
@@ -1,14 +1,15 @@
 from pystac import Catalog
 from odc.stac import load as odc_stac_load
 
-from typing import Tuple
+from typing import Tuple, Optional
 from xarray import DataArray
 
 from sdc.products import _ancillary as anc
 from sdc.products import _query as query
 
 
-def load_sanlc(bounds: Tuple[float, float, float, float]
+def load_sanlc(bounds: Tuple[float, float, float, float],
+               year: Optional[int] = None
                ) -> DataArray:
     """
     Loads the South African National Land Cover (SANLC) data product for an area of 
@@ -19,6 +20,11 @@ def load_sanlc(bounds: Tuple[float, float, float, float]
     bounds: tuple of float
         The bounding box of the area of interest in the format (minx, miny, maxx, maxy).
         Will be used to filter the STAC Catalog for intersecting STAC Collections.
+    year: int, optional
+        The year of the SANLC product to load. Default is None, which loads all
+        available years, which currently are:
+        - 2018
+        - 2020
     
     Returns
     -------
@@ -39,4 +45,10 @@ def load_sanlc(bounds: Tuple[float, float, float, float]
     ds = odc_stac_load(items=items, bands=bands, bbox=bounds, dtype='uint8',
                        **common_params)
     
-    return ds.nlc.squeeze()
+    if year is not None:
+        if year not in [2018, 2020]:
+            raise ValueError('The SANLC product is only available for the years 2018 '
+                             'and 2020.')
+        ds = ds.sel(time=f'{year}-01-01', method='nearest')
+    
+    return ds.nlc
