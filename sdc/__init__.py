d82afc694ba35402e178007ffbfe87fc872d0475
maawoo <marco.wolsza@uni-jena.de>
2023-12-11 15:28:34 +0100

    [sdc.products.s1] set `optimization.fuse.active` to False
    Will otherwise run into https://github.com/dask/distributed/issues/7816

--- sdc/__init__.py
+++ sdc/__init__.py
@@ -3,6 +3,7 @@ import xarray as xr
 from sdc.cluster import start_slurm_cluster
 
 dask.config.set({"array.rechunk.method": "p2p"})
+dask.config.set({"optimization.fuse.active": False})
 xr.set_options(keep_attrs=True)
 
 dask_client, cluster = start_slurm_cluster()
