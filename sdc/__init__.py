568c19398d30fd9c2d73e502147cb93cc8fa91c0
Marco Wolsza <marco.wolsza@uni-jena.de>
2024-10-24 11:31:19 +0200

    feat: increase the default chunk size of dask (#64)

--- sdc/__init__.py
+++ sdc/__init__.py
@@ -2,8 +2,9 @@ import dask
 import xarray as xr
 from sdc.cluster import start_slurm_cluster
 
-dask.config.set({"array.rechunk.method": "p2p"})
-dask.config.set({"optimization.fuse.active": False})
+dask.config.set({"array.rechunk.method": "p2p",
+                 "optimization.fuse.active": False,
+                 "array.chunk-size": "256MiB"})
 xr.set_options(keep_attrs=True)
 
 dask_client, cluster = start_slurm_cluster()
