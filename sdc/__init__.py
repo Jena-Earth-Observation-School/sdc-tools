af1c6da2a06a8196b68a16e388c25c92773450d6
Marco Wolsza <marco.wolsza@uni-jena.de>
2026-04-02 17:59:16 +0200

    feat: replace local `cluster` module with external `draco` package  (#91)
    * chore: relax python version requirement
    
    * feat: add draco dependency
    
    * feat: use draco, remove cluster.py

--- sdc/__init__.py
+++ sdc/__init__.py
@@ -1,10 +1,14 @@
 import dask
 import xarray as xr
-from sdc.cluster import start_slurm_cluster
+from draco import start_slurm_cluster
 
 dask.config.set({"array.rechunk.method": "p2p",
                  "optimization.fuse.active": False,
                  "array.chunk-size": "256MiB"})
 xr.set_options(keep_attrs=True)
 
-dask_client, cluster = start_slurm_cluster()
+dask_client, dask_cluster = start_slurm_cluster(processes=3, 
+                                                cores=12,
+                                                memory='18 GiB',
+                                                walltime='01:00:00',
+                                                reservation='SALDI')
