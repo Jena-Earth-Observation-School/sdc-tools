d5700197ec4b1e2f7af9655de34a26271ebd3539
maawoo <marco.wolsza@uni-jena.de>
2026-05-05 14:33:46 +0200

    feat: implement hidden option to customize requested slurm resources

--- sdc/__init__.py
+++ sdc/__init__.py
@@ -1,14 +1,11 @@
 import dask
 import xarray as xr
-from draco import start_slurm_cluster
+from ._cluster import start_cluster
+
 
 dask.config.set({"array.rechunk.method": "p2p",
                  "optimization.fuse.active": False,
                  "array.chunk-size": "256MiB"})
 xr.set_options(keep_attrs=True)
 
-dask_client, dask_cluster = start_slurm_cluster(processes=3, 
-                                                cores=12,
-                                                memory='18 GiB',
-                                                walltime='01:00:00',
-                                                reservation='SALDI')
+dask_client, dask_cluster = start_cluster()
